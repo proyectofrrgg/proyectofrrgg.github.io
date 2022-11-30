3ba4de8ce0065066eeb943ae355c53e41dbe07f6
proyectofrrgg <119136230+proyectofrrgg@users.noreply.github.com>
2022-11-30 02:03:16 -0600

    Add files via upload

---
+++ js/CtrlAlumnoNuevo.js
@@ -0,0 +1,63 @@
+import {
+  getAuth,
+  getFirestore
+} from "../lib/fabrica.js";
+import {
+  getString,
+  muestraError
+} from "../lib/util.js";
+import {
+  muestraAlumnos
+} from "./navegacion.js";
+import {
+  tieneRol
+} from "./seguridad.js";
+
+const daoAlumno =
+  getFirestore().
+    collection("Alumno");
+/** @type {HTMLFormElement} */
+const forma = document["forma"];
+getAuth().onAuthStateChanged(
+  protege, muestraError);
+
+/** @param {import(
+    "../lib/tiposFire.js").User}
+    usuario */
+async function protege(usuario) {
+  if (tieneRol(usuario,
+    ["Administrador"])) {
+    forma.addEventListener(
+      "submit", guarda);
+  }
+}
+
+/** @param {Event} evt */
+async function guarda(evt) {
+  try {
+    evt.preventDefault();
+    const formData =
+      new FormData(forma);
+    const codigo = getString(
+        formData, "codigo").trim();  
+    const nombre = getString(formData, "marca").trim();
+    const telefono = getString(formData, "precio").trim();
+    const grupo = getString(formData, "color").trim();
+    /**
+     * @type {
+        import("./tipos.js").
+                Alumno} */
+    const modelo = {
+      codigo,
+      marca,
+      precio,
+      color, 
+    };
+    await daoAlumno.
+      add(modelo);
+    muestraAlumnos();
+  } catch (e) {
+    muestraError(e);
+  }
+}
+
